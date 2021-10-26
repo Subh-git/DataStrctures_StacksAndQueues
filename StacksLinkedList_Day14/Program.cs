ca45c0e2e00567740491f8f79b7bf61dfb5187a9
Subhadeep <subhadeep0326@gmail.com>
2021-10-26 22:42:59 +0530

    Subhadeep adding data to the main

---
+++ StacksLinkedList_Day14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksLinkedList_Day14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
